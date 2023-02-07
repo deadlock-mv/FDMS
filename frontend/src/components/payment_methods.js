c5d0364c8c9cf73c4554b2e7aab9049bd71b693b
Abhishek MV <115617662+deadlock-mv@users.noreply.github.com>
2023-02-07 18:12:43 +0530

    frontend files added
    no node_modules

---
+++ frontend/src/components/payment_methods.js
@@ -0,0 +1,17 @@
+export default function Payment() {
+
+    return (
+        <div className="card container mx-auto mt-4">
+            <ul className="list-group list-group-flush">
+                <li href="#" className="list-group-item list-group-item-action">UPI</li>
+                <li href="#" className="list-group-item list-group-item-action">Credit/Debit Card</li>
+                <li href="#" className="list-group-item list-group-item-action">Wallets</li>
+                <li href="#" className="list-group-item list-group-item-action">A fourth link item</li>
+                <li className="list-group-item list-group-item-action disabled">A disabled link item</li>
+            </ul>
+        </div>
+
+        
+    );
+
+}; 
